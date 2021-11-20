6826a6e7a3b8eca4fed7588d76a3e026e41afd6e
Antonio Bento de Souza Neto <absn3@cin.ufpe.br>
2021-11-20 01:49:36 -0300

    mux PC source

---
+++ Mux_PC_src.v
@@ -0,0 +1,20 @@
+module Mux_PC_src (
+    input wire[2:0] PC_src,
+    input wire[31:0] ULA_out,
+    input wire[31:0] ALUOut_out,
+    input wire[31:0] jump,
+    input wire[31:0] negocioVermelho,//mudar nome n lembro oq faz
+    input wire[31:0] EPC_out,
+    
+    output wire[31:0] PC_in
+);
+
+    assign PC_in = (PC_src == 3'b000) ? ULA_out :
+                   (PC_src == 3'b001) ? ALUOut_out :
+                   (PC_src == 3'b010) ? jump :
+                   (PC_src == 3'b011) ? negocioVermelho :
+                   (PC_src == 3'b100) ? EPC_out: 
+                   32'bxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx; 
+
+    
+endmodule
